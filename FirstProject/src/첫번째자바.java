c37e792717c342e5d19fa656bf7608fb44562519
user <user@DESKTOP-KMQKUM1>
2024-11-18 14:25:16 +0900

    2024-11-18

---
+++ FirstProject/src/첫번째자바.java
@@ -0,0 +1,83 @@
+/*
+	1. 대문자 구분
+	2. 들여쓰기
+	3. 명령문이 종료 반드시 ;
+		; => 파이썬 / Kotlin / javascript (;을 사용하지 않는다.)
+	4. 자바는 { 가 옆에 붙는다.
+	5. 문법
+		데이터 저장 =====> 데이터 처리 (요청) =====> 화면 출력
+			|				|					|
+			|			연산자 / 제어문			명령프롬프트 : Application
+			|			===========				문법
+			|				|				브라우저 출력 : Web Application
+			|			  메소드					주력
+			|								모바일 출력	: mobile Application
+			|								Spring : Java / Kotlin	
+		한개 저장 (메모리): 변수/상수
+		여러개 저장 : 	배열
+					클래스
+		-----------------------------
+		프로그램 종료시에 사라진다
+		파일 / 오라클 => 영구적인 저장
+		
+		================> 기본문법
+		1. 기능을 모아서 저장
+		   ===
+			=> 단락 : 메소드
+			=> 클래스 / 객체 => 객체 지향 프로그램
+			=> 프로그램 유지 (예외처리)
+			---------------------------- 8장
+			=> 라이브러리 : 자바에 지원
+			=> 1차 응용프로그램
+		================> Web
+		1. HTML : 브라우저에서 실행
+				  자바는 브라우저에서는 일반 텍스트
+		   => 투박 => CSS / 자바스크립트
+		2. JSP (요청 => HTML => Java)
+				응답 => Java => HTML
+			| MVC
+			  ===
+			  Front / Back
+			   |	   | 자바 / 오라클 / Spring
+			  JS / TS
+			  =============================+ Full Stack
+		3. 통합 : Framework : Spring
+				 기본 틀 => 메인보드
+		4. 데이터 분석 : Python / Numpy / Pandas
+					  MatplotLib
+		5. NoSql => elasticesearch
+						| 오라클 역할
+		6. 배포 : AWS
+		===============================================================
+		Spring=Boot / JAP / MySQL / React-Quary
+		Redux / Vue3 / NextJS / TypeScript / NodeJs
+		
+		자바 / 오라클 / 알고리즘
+		HTML / CSS / JavaScript / JSP
+		Spring-Boot / Python / 데이터 분석
+		elasticsearch
+		==================================
+		Spring + MyBatis + JSP + JavaScript(Jquery)
+		
+		
+		
+		수업
+		월~목
+		1~6교시 : 수업
+		7교시 => 문제 풀이
+		8교시 => 문제 해결 / 하루 정리
+		==========================
+		금 ( 1주일 정리)
+ */
+public class 첫번째자바 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+		System.out.println("김진용");
+		{
+			
+		}
+	}
+	
+}
